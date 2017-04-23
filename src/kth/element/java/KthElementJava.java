00c1cc30601a1e2d882080f1ed9041cf567baf0b
Joshua Camacho <joshuajcamacho@gmail.com>
2017-04-23 15:04:39 -0700

    initial commit

---
+++ src/kth/element/java/KthElementJava.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kth.element.java;
+
+/**
+ *
+ * @author Josh
+ */
+public class KthElementJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
